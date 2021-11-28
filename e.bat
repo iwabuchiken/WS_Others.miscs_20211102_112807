3e671ed64cd4453bb903a75730bdca98f547bfeb
iwabuchiken <iwabuchi.k.2010@gmail.com>
2021-11-28 17:04:10 +0900

    (WS_Others.miscs_20211102_112807 : diary : towa) periodical updates

--- e.bat
+++ e.bat
@@ -1,1 +1,4 @@
-g pull https://github.com/iwabuchiken/WS_Others.miscs.git
+rem g pull https://github.com/iwabuchiken/WS_Others.miscs.git
+
+rem : update : 2021”N11ŒŽ22“ú10:59:14
+g pull https://github.com/iwabuchiken/WS_Others.miscs_20211102_112807
